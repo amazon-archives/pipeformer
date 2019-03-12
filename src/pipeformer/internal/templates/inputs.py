dc800514dac99b2d18a36014fbdd4aa89a86c914
mattsb42-aws <bullocm@amazon.com>
2019-03-11 19:41:13 -0700

    inputs template docs

--- src/pipeformer/internal/templates/inputs.py
+++ src/pipeformer/internal/templates/inputs.py
@@ -20,19 +20,36 @@ from . import project_tags
 
 
 def _secret_value(resource: Input, tags: Tags, cmk_arn: Ref) -> secretsmanager.Secret:
-    """"""
+    """Construct a Secrets Manager secret to store the input value.
+
+    :param Input resource:
+    :param Tags tags:
+    :param Ref cmk_arn:
+    :return: Constructed resource
+    :rtype: secretsmanager.Secret
+    """
     return secretsmanager.Secret(
         resource_name(secretsmanager.Secret, resource.name), KmsKeyId=cmk_arn, SecretString="REPLACEME", Tags=tags
     )
 
 
 def _standard_value(resource: Input) -> ssm.Parameter:
-    """"""
+    """Construct a Parameter Store parameter containing the input value.
+
+    :param Input resource: Input to store
+    :return: Constructed resource
+    :rtype: ssm.Parameter
+    """
     return ssm.Parameter(resource_name(ssm.Parameter, resource.name), Type="String", Value=resource.value)
 
 
 def build(project: Config) -> Template:
-    """"""
+    """Build an Inputs stack template from the provided project.
+
+    :param Config project: Source project
+    :return: Generated Inputs stack template
+    :rtype: Template
+    """
     inputs = Template(Description=f"Input values for pipeformer-managed project: {project.name}")
     cmk = inputs.add_parameter(Parameter(reference_name(resource_name(kms.Key, "Stack"), "Arn"), Type="String"))
 
